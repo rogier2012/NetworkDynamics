09dd2413225f08aff543e17705b83e70c43cce52
Rogier Monshouwer <r.monshouwer@student.utwente.nl>
2016-11-30 13:44:28 +0100

    added stub likes system

--- test.py
+++ test.py
@@ -31,20 +31,24 @@ def youtube_search(options):
 
   # Add each result to the appropriate list, and then display the lists of
   # matching videos, channels, and playlists.
-  for search_result in search_response.get("items", []):
-    if search_result["id"]["kind"] == "youtube#video":
-      videos.append("%s (%s)" % (search_result["snippet"]["title"],
-                                 search_result["id"]["videoId"]))
-    elif search_result["id"]["kind"] == "youtube#channel":
-      channels.append("%s (%s)" % (search_result["snippet"]["title"],
-                                   search_result["id"]["channelId"]))
-    elif search_result["id"]["kind"] == "youtube#playlist":
-      playlists.append("%s (%s)" % (search_result["snippet"]["title"],
-                                    search_result["id"]["playlistId"]))
+  # for search_result in search_response.get("items", []):
+  #   if search_result["id"]["kind"] == "youtube#video":
+  #     videos.append("%s (%s)" % (search_result["snippet"]["title"],
+  #                                search_result["id"]["videoId"]))
+  #   elif search_result["id"]["kind"] == "youtube#channel":
+  #     channels.append("%s (%s)" % (search_result["snippet"]["title"],
+  #                                  search_result["id"]["channelId"]))
+  #   elif search_result["id"]["kind"] == "youtube#playlist":
+  #     playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+  #                                   search_result["id"]["playlistId"]))
+  #
+  # print("Videos:\n", "\n".join(videos), "\n")
+  # print("Channels:\n", "\n".join(channels), "\n")
+  # print("Playlists:\n", "\n".join(playlists), "\n")
+  print(type(search_response))
+  for search_result in search_response.get("items",[]):
+    print(search_result)
 
-  print("Videos:\n", "\n".join(videos), "\n")
-  print("Channels:\n", "\n".join(channels), "\n")
-  print("Playlists:\n", "\n".join(playlists), "\n")
 
 
 if __name__ == "__main__":
---
+++ youtube_data_handler.py
@@ -0,0 +1,49 @@
+import os
+import json
+import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
+
+def difference_likes_dislikes():
+    result = {}
+    json_data1 = open("data/spotify_top100/20151109_1800_data.json").read()
+    spotify_data = json.loads(json_data1)
+    spotify_tracks = spotify_data.get("tracks").get("items", [])
+    for track in spotify_tracks:
+        track_name = track.get("track", {}).get("name", "")
+        result[track_name] = {"index": spotify_tracks.index(track), "likes": []}
+
+    for filename in os.listdir("data/youtube_top100"):
+        if (filename.endswith(".json")):
+            year = int(filename[:4])
+            month = int(filename[4:6])
+            day = int(filename[6:8])
+            date = datetime.date(year,month,day)
+            datenum = matplotlib.dates.date2num(date)
+            json_data = open("data/youtube_top100/" + filename).read()
+            youtube_data = json.loads(json_data)
+            for video in youtube_data:
+
+                statistics = video.get("statistics", {})
+                likes = statistics.get("likeCount")
+                dislikes = statistics.get("dislikeCount")
+                for k,v in result.items():
+                    if v.get("index",-1) == youtube_data.index(video):
+                        v.get("likes").append((int(likes)-int(dislikes)))
+
+    return result
+
+# print(difference_likes_dislikes())
+data = difference_likes_dislikes().popitem()
+# print(data[1].get("likes").keys())
+begin = datetime.date(2015,11,9)
+end = datetime.date(2016,11,28)
+delta = datetime.timedelta(days=1)
+dates = drange(begin,end,delta)
+
+
+
+plt.plot(data[1].get("likes"))
+plt.title(data[0])
+plt.show()
