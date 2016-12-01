fb08558133abfe37de3e9541e1b151270bcf5ace
Rogier Monshouwer <r.monshouwer@student.utwente.nl>
2016-12-01 15:18:44 +0100

    find cascades function

--- test1.py
+++ test1.py
@@ -1,18 +1,29 @@
 import json
+import os
+# json_data1=open("data/spotify_top100/20151109_1800_data.json").read()
+# spotify_data = json.loads(json_data1)
+# json_data2=open("data/youtube_top100/20161128_1800_data.json").read()
+# youtube_data = json.loads(json_data2)
+# print(len(youtube_data))
+# # print(youtube_data[0].get("snippet",{}).get("title"),"")
+# spotify_tracks = spotify_data.get("tracks").get("items", [])
+# for track in spotify_tracks:
+#     track_name = track.get("track",{}).get("name","")
+#     video_name = youtube_data[spotify_tracks.index(track)].get("snippet",[]).get("title","")
+#     statistics = youtube_data[spotify_tracks.index(track)].get("statistics",{})
+#     likes = statistics.get("likeCount")
+#     dislikes = statistics.get("dislikeCount")
+#     # print(track_name + " with " + str(likes) + " likes and "  + str(dislikes) + " dislikes")
+#
+#     print(track_name + " youtube: " + video_name + " index " + str(spotify_tracks.index(track)))
+counter = 0
+folder = "youtube_top100"
 
-json_data1=open("data/spotify_top100/20151109_1800_data.json").read()
-spotify_data = json.loads(json_data1)
-json_data2=open("data/youtube_top100/20161128_1800_data.json").read()
-youtube_data = json.loads(json_data2)
-print(len(youtube_data))
-# print(youtube_data[0].get("snippet",{}).get("title"),"")
-spotify_tracks = spotify_data.get("tracks").get("items", [])
-for track in spotify_tracks:
-    track_name = track.get("track",{}).get("name","")
-    video_name = youtube_data[spotify_tracks.index(track)].get("snippet",[]).get("title","")
-    statistics = youtube_data[spotify_tracks.index(track)].get("statistics",{})
-    likes = statistics.get("likeCount")
-    dislikes = statistics.get("dislikeCount")
-    # print(track_name + " with " + str(likes) + " likes and "  + str(dislikes) + " dislikes")
 
-    print(track_name + " youtube: " + video_name + " index " + str(spotify_tracks.index(track)))
+for filename in os.listdir("data/"+folder):
+    counter += 1
+    json_data2 = open("data/"+folder+"/"+str(filename)).read()
+    youtube_data = json.loads(json_data2)
+    print(str(counter) + ". " + filename)
+    for video in youtube_data:
+        print(str(youtube_data.index(video)) + " "+  video.get("snippet").get("title"))
--- youtube_data_handler.py
+++ youtube_data_handler.py
@@ -6,67 +6,97 @@ from pprint import pprint
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
-import operator
 
-def plot_hotline_bling():
-    likes_per_day = {}
-    # for i in range(50):
-    likes_per_day = get_likes_dislikes(0)
-    data = []
-    data_with_date = {}
-    for k,v1,v2 in likes_per_day:
-        total = v1 + v2
-        diff = v1 - v2
-        data.append((total,diff))
-        data_with_date[total] = k
+def find_cascades():
+    cascades = dict()
+    for i in range(100):
+        likes_per_day,song_name = get_likes_dislikes(i,folder="youtube_top100")
+        if len(likes_per_day) > 0:
+            data = []
+            data_with_date = {}
+            for k,v1,v2 in likes_per_day:
+                total = v1 + v2
+                diff = v1 - v2
+                data.append((total,diff))
+                data_with_date[total] = k
+            diff_data = []
+            diff_data_with_date = []
+            previous = 0
+            for k,v in data:
+                diff_data.append((k,v-previous))
+                diff_data_with_date.append((data_with_date[k],v-previous))
+                previous = v
+            del diff_data[0]
+            x, y = (zip(*diff_data))
+            x = np.array(x)
+            y = np.array(y)
+            m, b = np.polyfit(x, y, 1)
+            cascades[song_name] =  m > 0
+    return cascades
 
-    diff_data = []
-    diff_data_with_date = []
-    previous = 0
-    for k,v in data:
-        diff_data.append((k,v-previous))
-        diff_data_with_date.append((data_with_date[k],v-previous))
-        previous = v
-    del diff_data[0]
-    print(*diff_data)
-    plt.plot(*zip(*diff_data))
-    x, y = (zip(*diff_data))
-    x = np.array(x)
-    y = np.array(y)
-    m, b = np.polyfit(x, y, 1)
-    # plt.plot(x, y, '.')
-    plt.plot(x, m * x + b, '-')
-    plt.title("Hotline Bling")
-    plt.show()
+def plot_popularity_song():
+    likes_per_day,song_name = get_likes_dislikes(47,folder="youtube_top100")
+    if len(likes_per_day) > 0:
+        data = []
+        data_with_date = {}
+        for k,v1,v2 in likes_per_day:
+            total = v1 + v2
+            diff = v1 - v2
+            data.append((total,diff))
+            data_with_date[total] = k
+
+        diff_data = []
+        diff_data_with_date = []
+        previous = 0
+        for k,v in data:
+            diff_data.append((k,v-previous))
+            diff_data_with_date.append((data_with_date[k],v-previous))
+            previous = v
+        del diff_data[0]
+        plt.plot(*zip(*diff_data))
+        x, y = (zip(*diff_data))
+        x = np.array(x)
+        y = np.array(y)
+        m, b = np.polyfit(x, y, 1)
+        # plt.plot(x, y, '.')
+        plt.plot(x, m * x + b, '-')
+        plt.title(song_name)
+        plt.show()
 
 
-def get_likes_dislikes(index):
+
+def get_likes_dislikes(index,folder="youtube_top100"):
     result = []
-    counter =0
-    for file in os.listdir("data/youtube_top100/"):
+    counter = 0
+    song_name = ""
+    for file in os.listdir("data/"+folder+"/"):
         if (counter < 300):
-            json_data1 = open("data/youtube_top100/" + file).read()
+            json_data1 = open("data/"+folder+"/" + file).read()
             youtube = json.loads(json_data1)
-            entry = youtube[index]
-            # if entry['snippet']['title'] == "Drake - Hotline Bling":
-            if str(file) == "20160530_1800_data.json":
-                (key, likes,dislikes) = result[len(result)-1]
-                step = (int(entry['statistics']['likeCount']) - likes)//4
-                dislike_step = (int(entry['statistics']['dislikeCount']) - dislikes)//4
-                result.append(("20160527", likes+step, dislikes + dislike_step ))
-                result.append(("20160528", likes+2*step, dislikes+ 2*dislike_step ))
-                result.append(("20160529", likes+3*step, dislikes+ 3*dislike_step ))
-            elif str(file) == "20160808_1800_data.json":
-                (key, likes,dislikes) = result[len(result)-1]
-                step = (int(entry['statistics']['likeCount']) - likes)//3
-                dislike_step = (int(entry['statistics']['dislikeCount']) - dislikes)//3
-                result.append(("20160806", likes+step, dislikes + dislike_step ))
-                result.append(("20160807", likes+2*step, dislikes+ 2*dislike_step ))
-            result.append((file.replace("_1800_data.json", ""),int(entry['statistics']['likeCount']), int(entry['statistics']['dislikeCount'])))
+            if (index < len(youtube)):
+                entry = youtube[index]
+                if song_name == "": song_name = entry.get("snippet",{}).get("title","")
+                # if entry['snippet']['title'] == "Drake - Hotline Bling":
+                if str(file) == "20160530_1800_data.json":
+                    (key, likes,dislikes) = result[len(result)-1]
+                    step = (int(entry['statistics']['likeCount']) - likes)//4
+                    dislike_step = (int(entry['statistics']['dislikeCount']) - dislikes)//4
+                    result.append(("20160527", likes+step, dislikes + dislike_step ))
+                    result.append(("20160528", likes+2*step, dislikes+ 2*dislike_step ))
+                    result.append(("20160529", likes+3*step, dislikes+ 3*dislike_step ))
+                elif str(file) == "20160808_1800_data.json":
+                    (key, likes,dislikes) = result[len(result)-1]
+                    step = (int(entry['statistics']['likeCount']) - likes)//3
+                    dislike_step = (int(entry['statistics']['dislikeCount']) - dislikes)//3
+                    result.append(("20160806", likes+step, dislikes + dislike_step ))
+                    result.append(("20160807", likes+2*step, dislikes+ 2*dislike_step ))
+                result.append((file.replace("_1800_data.json", ""),int(entry['statistics']['likeCount']), int(entry['statistics']['dislikeCount'])))
         counter += 1
 
-    return result
+    return result,song_name
 
 
-plot_hotline_bling()
+for k,v in find_cascades().items():
+    if v:
+        print(k)
+plot_popularity_song()
